4f932bb924e1cb3b599ebee7f5e0acd9a9038312
Henry.Liang <henryliang3027@gmail.com>
2024-04-11 14:18:29 +0800

    refactor: :recycle: Only retain the implementation for the Windows platform.

--- quick_blue/lib/quick_blue.dart
+++ quick_blue/lib/quick_blue.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:quick_blue_linux/quick_blue_linux.dart';
-import 'package:quick_blue_platform_interface/method_channel_quick_blue.dart';
 import 'package:quick_blue_platform_interface/quick_blue_platform_interface.dart';
 
 import 'models.dart';
@@ -21,14 +18,9 @@ QuickBluePlatform get _instance {
   if (_manualDartRegistrationNeeded) {
     // Only do the initial registration if it hasn't already been overridden
     // with a non-default instance.
-    if (Platform.isAndroid ||
-        Platform.isIOS ||
-        Platform.isWindows ||
-        Platform.isMacOS) {
-      QuickBluePlatform.instance = MethodChannelQuickBlue();
-    } else if (Platform.isLinux) {
-      QuickBluePlatform.instance = QuickBlueLinux();
-    }
+
+    QuickBluePlatform.instance = MethodChannelQuickBlue();
+
     _manualDartRegistrationNeeded = false;
   }
 
@@ -45,7 +37,6 @@ class QuickBlue {
 
   static void setLogger(QuickLogger logger) => _platform.setLogger(logger);
 
- 
   static Future<bool> isBluetoothAvailable() =>
       _platform.isBluetoothAvailable();
 
@@ -55,7 +46,7 @@ class QuickBlue {
 
   static Stream<BlueScanResult> get scanResultStream {
     return _platform.scanResultStream
-      .map((item) => BlueScanResult.fromMap(item));
+        .map((item) => BlueScanResult.fromMap(item));
   }
 
   static void connect(String deviceId) => _platform.connect(deviceId);
@@ -66,27 +57,38 @@ class QuickBlue {
     _platform.onConnectionChanged = onConnectionChanged;
   }
 
-  static void discoverServices(String deviceId) => _platform.discoverServices(deviceId);
+  static void discoverServices(String deviceId) =>
+      _platform.discoverServices(deviceId);
 
   static void setServiceHandler(OnServiceDiscovered? onServiceDiscovered) {
     _platform.onServiceDiscovered = onServiceDiscovered;
   }
 
-  static Future<void> setNotifiable(String deviceId, String service, String characteristic, BleInputProperty bleInputProperty) {
-    return _platform.setNotifiable(deviceId, service, characteristic, bleInputProperty);
+  static Future<void> setNotifiable(String deviceId, String service,
+      String characteristic, BleInputProperty bleInputProperty) {
+    return _platform.setNotifiable(
+        deviceId, service, characteristic, bleInputProperty);
   }
 
   static void setValueHandler(OnValueChanged? onValueChanged) {
     _platform.onValueChanged = onValueChanged;
   }
 
-  static Future<void> readValue(String deviceId, String service, String characteristic) {
+  static Future<void> readValue(
+      String deviceId, String service, String characteristic) {
     return _platform.readValue(deviceId, service, characteristic);
   }
 
-  static Future<void> writeValue(String deviceId, String service, String characteristic, Uint8List value, BleOutputProperty bleOutputProperty) {
-    return _platform.writeValue(deviceId, service, characteristic, value, bleOutputProperty);
+  static Future<void> writeValue(
+      String deviceId,
+      String service,
+      String characteristic,
+      Uint8List value,
+      BleOutputProperty bleOutputProperty) {
+    return _platform.writeValue(
+        deviceId, service, characteristic, value, bleOutputProperty);
   }
 
-  static Future<int> requestMtu(String deviceId, int expectedMtu) => _platform.requestMtu(deviceId, expectedMtu);
+  static Future<int> requestMtu(String deviceId, int expectedMtu) =>
+      _platform.requestMtu(deviceId, expectedMtu);
 }
